c100d90333b51b37f1898124bb6d4faf77618bdf
gubei <130200079+ducloudl@users.noreply.github.com>
2026-07-03 21:48:05 +0800

    fix: update js/app.js

--- js/app.js
+++ js/app.js
@@ -10,31 +10,28 @@ import { Templates } from './templates.js';
 class App {
   constructor() {
     this.board = new Board();
-    this.uiManager = new UIManager(this.board, null, null, null);
     this.templates = new Templates(this.board, null);
+    this.uiManager = new UIManager(this.board, null, null, this.templates);
     this.tools = new Tools(this.board, this.uiManager);
     this.state = new StateManager(this.board);
     
-    // Wire up dependencies
+    // Wire up dependencies after all constructed
     this.uiManager.board = this.board;
     this.uiManager.tools = this.tools;
     this.uiManager.state = this.state;
-    this.uiManager.templates = this.templates;
     this.tools.uiManager = this.uiManager;
     this.templates.tools = this.tools;
-    
-    this.init();
   }
   
   async init() {
     await this.uiManager.initialize();
+    this.templates.loadTemplates();
     this.board.render();
     this.tools.selectTool('pen');
     this.state.restoreState();
   }
 }
 
-// Start the app when DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
   window.app = new App();
 });
